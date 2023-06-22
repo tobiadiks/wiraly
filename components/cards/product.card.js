802d942189798f96729bb871ce2ef8ba4607916f
Adeleke Oluwatobi <57203812+tobiadiks@users.noreply.github.com>
2023-06-22 21:41:21 +0100

    Update product.card.js

--- components/cards/product.card.js
+++ components/cards/product.card.js
@@ -8,7 +8,7 @@ export default function ProductCard(props) {
     return (
         <div onClick={()=>router.push(`/product/${props.id}`)} className='rounded-lg ring-2 cursor-pointer snap-center flex-none w-full ring-black bg-white'>
             {/* eslint-disable-next-line @next/next/no-img-element  */}
-            <div className='border-b-2 z-10 border-b-black p-2 relative h-32'><img alt='' className="rounded-t-lg text-xs object-fit:cover" src={props.src} />
+            <div className='border-b-2 z-10 border-b-black p-2 relative h-full w-full'><img alt='' className="rounded-t-lg text-xs object-fit:cover" src={props.src} />
                 <div className="z-20 absolute top-2 left-2 bg-white text-xs px-2 py-1 text-center"><sup className="font-bold">Sold</sup>{props.sold}/{props.total}</div>
             </div>
             <div className='p-2 flex justify-between'>
